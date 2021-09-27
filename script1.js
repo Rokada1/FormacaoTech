c8f6402bfe5d2f05175365a3684b7d5bf645d8fd
Rokada1 <rcokada7@gmail.com>
2021-09-26 23:40:03 -0300

    Tentativa mascara cpf

--- script1.js
+++ script1.js
@@ -4,19 +4,16 @@ const masks = {
         return value
         .replace(/\D/g, '')
         .replace(/(\d{3})(\d)/, '$1.$2')
+        .replace(/(\d{3})(\d)/, '$1.$2')
+        .replace(/(\d{3})(\d{1,2})/, '$1-$2')
+        .replace(/(-\d{2})\d+?$/, '$1')
     }
 }
 
-
-
-
-
-
-
-document.querySelectorAll('input').forEach($input){
+document.querySelectorAll('input').forEach(($input) =>{
     const field = $input.dataset.js
 
-    $input.addEventListenner('input', (e){
+    $input.addEventListenner('input', (e) => {
         e.target.value = masks[field](e.target.value)
     },false)
-}
+})
